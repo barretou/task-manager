68ad40753b268c98fcdb9fa0c1253d15c6046649
gustavo barreto <gustavo.barreto@aqtech.com>
2025-03-04 01:46:27 -0300

    adds bcrypt

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
@@ -6,13 +6,19 @@ export class TaskService {
   private tasks: TaskDto[] = [];
 
   getTasks(): TaskDto[] {
+    if (this.tasks.length === 0) {
+      throw new HttpException('Tasks is empty', HttpStatus.NO_CONTENT);
+    }
     return this.tasks;
   }
 
-  getTaskById(id: string): TaskDto | undefined {
+  getTaskById(id: string): TaskDto {
     const task = this.tasks.find((task) => task.id === id);
     if (!task) {
-      throw new NotFoundException('Task not found');
+      throw new HttpException(
+        `Task with id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
     }
     return task;
   }
@@ -25,7 +31,10 @@ export class TaskService {
   updateTask(id: string, task: TaskDto) {
     const index = this.tasks.findIndex((task) => task.id === id);
     if (index === -1) {
-      throw new NotFoundException('Task not found');
+      throw new HttpException(
+        `Task with id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
     }
     this.tasks[index] = task;
     return task;
@@ -34,7 +43,10 @@ export class TaskService {
   deleteTask(id: string) {
     const index = this.tasks.findIndex((task) => task.id === id);
     if (index === -1) {
-      throw new NotFoundException('Task not found');
+      throw new HttpException(
+        `Task with id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
     }
     const deletedTask = this.tasks.splice(index, 1);
     return deletedTask[0];
